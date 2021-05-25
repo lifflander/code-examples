46d237329dfd480fb389b5c541d23ad6f2bcc7c6
Jonathan Lifflander <jliffla@sandia.gov>
2021-05-25 13:39:36 -0700

    Calculate stdev

--- papers/kokkos-core-tpds-2021/cgsolve-graphs/read-output.pl
+++ papers/kokkos-core-tpds-2021/cgsolve-graphs/read-output.pl
@@ -8,10 +8,38 @@ my @graph = (0, 1);
 
 my $num = 20;
 
+sub average{
+    my($data) = @_;
+    if (not @$data) {
+        die("Empty arrayn");
+    }
+    my $total = 0;
+    foreach (@$data) {
+        $total += $_;
+    }
+    my $average = $total / @$data;
+    return $average;
+}
+sub stdev {
+    my($data) = @_;
+    if(@$data == 1){
+        return 0;
+    }
+    my $average = &average($data);
+    my $sqtotal = 0;
+    foreach(@$data) {
+        $sqtotal += ($average-$_) ** 2;
+    }
+    my $std = ($sqtotal / (@$data-1)) ** 0.5;
+    return $std;
+}
+
 foreach my $use_graph (@graph) {
     open my $out, '>', "graph-2.$use_graph.dat";
     foreach my $n (@sizes) {
-        my ($gflops, $nrows, $iter, $time) = (0.0, 0, 0, 0.0);
+        my ($nrows, $iter) = (0.0, 0, 0, 0.0);
+        my @time;
+        my @gflops;
         foreach my $i (0..$num) {
             my $filename = "output-2/run.$i.$use_graph.$n.out";
             print "Parsing configuration: N=$n use_graph=$use_graph; $filename\n";
@@ -22,7 +50,7 @@ foreach my $use_graph (@graph) {
                     if ($1 == 0) {
                         print "failure to parse gflops: $filename\n";
                     }
-                    $gflops += $1;
+                    push @gflops, $1;
                 }
                 if (/nrows = ([0-9]+)/) {
                     if ($nrows != 0 && $nrows != $1) {
@@ -39,15 +67,17 @@ foreach my $use_graph (@graph) {
                         print "failure to parse time: $filename\n";
                     }
                     $iter = $1;
-                    $time += $2;
+                    push @time, $2;
                 }
             }
-            print "$gflops, $nrows, $iter, $time\n";
+            print "@gflops, $nrows, $iter, @time\n";
             close $file;
         }
-        my $avgtime = $time/$num;
-        my $avggflops = $gflops/$num;
-        print $out "$n $nrows $iter $avgtime $avggflops\n";
+        my $avgtime = &average(@time);
+        my $avggflops = &average(@gflops);
+        my $stdtime = &stdev(@time);
+        my $stdgflops = &stdev(@gflops);
+        print $out "$n $nrows $iter $avgtime $stdtime $avggflops $stdgflops\n";
 
     }
     close $out;
